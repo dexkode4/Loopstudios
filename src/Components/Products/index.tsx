b664d8f592d466de62cf4cec9902fe8ec54ba93a
dexkode4 <dexkode4@gmail.com>
2021-02-07 01:13:57 +0100

    responsiveness for products section

--- src/Components/Products/index.tsx
+++ src/Components/Products/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Product from './Product';
 import styles from './Products.module.scss';
-import {productImagesSchema} from '../../assets/img/mobile';
+import { productImagesSchema } from '../../assets/img/mobile';
+import { productImagesSchemaDesktop } from '../../assets/img/desktop';
 import Button from '../Button';
 
 type ProductsProps = {
@@ -9,15 +10,34 @@ type ProductsProps = {
 }
 
 function Products({ id }: ProductsProps) {
+    const [imageSchema, setImageSchema] = useState(productImagesSchema);
+    const [displayWidth, setDisplaywidth] = useState(window.innerWidth);
+    
+    const handleResize = () => {
+        setDisplaywidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        if(displayWidth > 700){
+            setImageSchema(productImagesSchemaDesktop)
+        }
+        else {
+            setImageSchema(productImagesSchema) 
+        }
+    },[displayWidth])
+
     return (
         <div className={styles.products} id={id}>
             <header className={styles.productsHeader}>OUR CREATIONS</header>
-            {
-                productImagesSchema.map(({image,title}) => (
-                    <Product imgSrc={image} title={title}/>
-                ))
-            }
-            <Button text="SEE ALL"/>
+            <div className={styles.productsContainer}>
+                {
+                    imageSchema.map(({ image, title },index) => (
+                        <Product imgSrc={image} title={title} key={index} />
+                    ))
+                }
+            </div>
+            <Button text="SEE ALL" />
         </div>
     )
 }
---
+++ src/assets/img/desktop/index.ts
@@ -0,0 +1,48 @@
+import Hero from './image-hero.jpg';
+import InteractiveImg from './image-interactive.jpg';
+import DeepEarthImg from './image-deep-earth.jpg';
+import NightArcadeImg from './image-night-arcade.jpg';
+import SoccerTeamImg from './image-soccer-team.jpg';
+import CarImg from './image-grid.jpg';
+import PocketBorealisImg from './image-pocket-borealis.jpg';
+import FromAboveImg from './image-from-above.jpg';
+import CuriosityImg from './image-curiosity.jpg';
+import FishEyeImg from './image-fisheye.jpg';
+
+
+export { Hero, InteractiveImg, DeepEarthImg };
+
+export const productImagesSchemaDesktop = [
+    {
+        image: DeepEarthImg,
+        title: ["Deep", "earth"]
+    },
+    {
+        image: NightArcadeImg,
+        title: ["Night", "arcade"]
+    },
+    {
+        image: SoccerTeamImg,
+        title: ["Soccer", "team vr"]
+    },
+    {
+        image: CarImg,
+        title: ["The", "grid"]
+    },
+    {
+        image: FromAboveImg,
+        title: ["from up", "above vr"]
+    },
+    {
+        image: PocketBorealisImg,
+        title: ["Pocket", "borealis"]
+    },
+    {
+        image: CuriosityImg,
+        title: ["The", "curiosity"]
+    },
+    {
+        image: FishEyeImg,
+        title: ["Make it", "fisheye"]
+    },    
+]
